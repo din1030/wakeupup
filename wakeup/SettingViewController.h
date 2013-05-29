73dd0c82d8fdf756277d531fb1ab490497118a44
Din cheng <din1030@gmail.com>
2013-05-30 01:01:26 +0800

    FB login

---
+++ wakeup/SettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.h
+//  wakeup
+//
+//  Created by din1030 on 13/5/29.
+//  Copyright (c) 2013年 din1030. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingViewController : UITableViewController
+
+@end
