f4537fcec0de2bd625a0a8c5ab96cf42f9e1f66b
Din cheng <din1030@gmail.com>
2013-06-03 20:05:04 +0800

    0603

---
+++ wakeup/BadgeListViewController.h
@@ -0,0 +1,13 @@
+//
+//  BadgeListViewController.h
+//  wakeup
+//
+//  Created by din1030 on 13/6/3.
+//  Copyright (c) 2013年 din1030. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BadgeListViewController : UIViewController
+
+@end
---
+++ wakeup/BadgeListViewController.m
@@ -0,0 +1,38 @@
+//
+//  BadgeListViewController.m
+//  wakeup
+//
+//  Created by din1030 on 13/6/3.
+//  Copyright (c) 2013年 din1030. All rights reserved.
+//
+
+#import "BadgeListViewController.h"
+
+@interface BadgeListViewController ()
+
+@end
+
+@implementation BadgeListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
