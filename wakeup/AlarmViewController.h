118171636e31c6a12d3b6e390bef492110dd1449
Toby <s81375@gmail.com>
2013-06-05 13:21:24 +0800

    add alarm.

---
+++ wakeup/AlarmViewController.h
@@ -0,0 +1,13 @@
+//
+//  AlarmViewController.h
+//  wakeup
+//
+//  Created by Toby Hsu on 13/6/5.
+//  Copyright (c) 2013年 din1030. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlarmViewController : UIViewController
+
+@end
---
+++ wakeup/AlarmViewController.m
@@ -0,0 +1,38 @@
+//
+//  AlarmViewController.m
+//  wakeup
+//
+//  Created by Toby Hsu on 13/6/5.
+//  Copyright (c) 2013年 din1030. All rights reserved.
+//
+
+#import "AlarmViewController.h"
+
+@interface AlarmViewController ()
+
+@end
+
+@implementation AlarmViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
